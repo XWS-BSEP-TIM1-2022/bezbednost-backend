76d3c084291fe383fa82280c195c04b2ea6807af
Mihajlo Mitić <mihajlomiticmiki@gmail.com>
2022-03-24 17:51:31 +0100

    initial project

---
+++ src/main/java/xwsbsep/bezbednostbackend/BezbednostBackendApplication.java
@@ -0,0 +1,13 @@
+package xwsbsep.bezbednostbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BezbednostBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BezbednostBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/xwsbsep/bezbednostbackend/BezbednostBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package xwsbsep.bezbednostbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BezbednostBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
